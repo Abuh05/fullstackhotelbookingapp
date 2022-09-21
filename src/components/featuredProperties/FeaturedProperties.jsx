57d5115044feadaad3e57b75cd617a7f5ea9b8fa
Abuh Solomon <53456402+Abuh05@users.noreply.github.com>
2022-09-21 18:04:16 +0200

    update

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,50 @@
+import './FeaturedProperties.css'
+
+const FeaturedProperties = () => {
+  return (
+    <div className='fp'>
+        <div className="fpItem">
+        <img src="https://images.pexels.com/photos/3201763/pexels-photo-3201763.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""  className='pFImg'/>
+        <span className='fpName'>Aparthotel Stare Miasto</span>
+        <span className='fpCity'>Madrid</span>
+        <span className='fpPrice'>Starting from $120</span>
+        <div className="fpRating">
+            <button>8.9</button>
+            <span>Excellent</span>
+        </div>
+        </div>
+        <div className="fpItem">
+        <img src="https://images.pexels.com/photos/1743231/pexels-photo-1743231.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""  className='pFImg'/>
+        <span className='fpName'>Aparthotel Stare Miasto</span>
+        <span className='fpCity'>Madrid</span>
+        <span className='fpPrice'>Starting from $120</span>
+        <div className="fpRating">
+            <button>8.9</button>
+            <span>Excellent</span>
+        </div>
+        </div>
+        <div className="fpItem">
+        <img src="https://images.pexels.com/photos/3634741/pexels-photo-3634741.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""  className='pFImg'/>
+        <span className='fpName'>Aparthotel Stare Miasto</span>
+        <span className='fpCity'>Madrid</span>
+        <span className='fpPrice'>Starting from $120</span>
+        <div className="fpRating">
+            <button>8.9</button>
+            <span>Excellent</span>
+        </div>
+        </div>
+        <div className="fpItem">
+        <img src="https://images.pexels.com/photos/2029719/pexels-photo-2029719.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""  className='pFImg'/>
+        <span className='fpName'>Aparthotel Stare Miasto</span>
+        <span className='fpCity'>Madrid</span>
+        <span className='fpPrice'>Starting from $120</span>
+        <div className="fpRating">
+            <button>8.9</button>
+            <span>Excellent</span>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedProperties
--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -11,31 +11,31 @@ const PropertyList = () => {
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://images.pexels.com/photos/1209978/pexels-photo-1209978.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""  className='pListImg'/>
+            <img src="https://images.pexels.com/photos/1434580/pexels-photo-1434580.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""  className='pListImg'/>
             <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233 hotels</h2>
+                <h1>Apartments</h1>
+                <h2>211 Apartments</h2>
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://images.pexels.com/photos/1209978/pexels-photo-1209978.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""  className='pListImg'/>
+            <img src="https://images.pexels.com/photos/1366957/pexels-photo-1366957.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""  className='pListImg'/>
             <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233 hotels</h2>
+                <h1>Resorts</h1>
+                <h2>253 Resorts</h2>
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://images.pexels.com/photos/1209978/pexels-photo-1209978.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""  className='pListImg'/>
+            <img src="https://images.pexels.com/photos/219692/pexels-photo-219692.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""  className='pListImg'/>
             <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233 hotels</h2>
+                <h1>Villas</h1>
+                <h2>233 Villas</h2>
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://images.pexels.com/photos/1209978/pexels-photo-1209978.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""  className='pListImg'/>
+            <img src="https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""  className='pListImg'/>
             <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233 hotels</h2>
+                <h1>Cabins</h1>
+                <h2>233 cabins</h2>
             </div>
         </div>
     </div>
